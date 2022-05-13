2b6db0b388e192fc537f274c2bf67bceab9eb3f1
Joseph Linares <js.linares@uniandes.edu.co>
2022-05-12 20:43:59 -0500

    Adelanto Reto 4

--- App/model.py
+++ App/model.py
@@ -24,18 +24,82 @@
  * Dario Correal - Version inicial
  """
 
-
-import config as cf
+import config
+from DISClib.ADT import graph as gr
+from DISClib.ADT import map as m
 from DISClib.ADT import list as lt
-from DISClib.ADT import map as mp
-from DISClib.DataStructures import mapentry as me
-from DISClib.Algorithms.Sorting import shellsort as sa
-assert cf
+from DISClib.Algorithms.Graphs import scc
+from DISClib.Algorithms.Graphs import dijsktra as djk
+from DISClib.Algorithms.Graphs import dfs as dfs
+from DISClib.Algorithms.Graphs import bfs as bfs
+from DISClib.Utils import error as error
+assert config
 
 """
 Se define la estructura de un catálogo de videos. El catálogo tendrá dos listas, una para los videos, otra para las categorias de
 los mismos.
 """
+
+def printMenu():
+    print("\n")
+    print("*******************************************")
+    print("Bienvenido")
+    print("1- Inicializar Analizador")
+    print("2- Cargar información de Toronto Bikeshare")
+    print("3- Comparar bicicletas para las estaciones con más viajes de origen.")
+    print("4- Reconocer los componentes fuertemente conectado del sistema")
+    print("5- Planear una ruta rápida para el usuario.")
+    print("6- Reportar rutas en un rango de fechas para los usuarios anuales")
+    print("7- Planear el mantenimiento preventivo de bicicletas")
+    print("8-  La estación más frecuentada por los visitantes")
+    print("0- Salir")
+    print("*******************************************")
+
+cont = None
+
+def thread_cycle():
+    while True:
+        printMenu()
+        inputs = input('Seleccione una opción para continuar\n>')
+
+        if int(inputs) == 1:
+            print("Inicializando....")
+
+            #cont = controller.init()
+
+        elif int(inputs) == 2:
+            #Requerimiento sin parametros#
+            pass
+
+        elif int(inputs) == 3:
+            intiStation = input('Nombre estación de inicio: ')
+            diponibility = input('Disponibilidad: ')
+            minRuteStops = int(input('Número minimo de estaciones de parada para la ruta: '))
+            maxNumRutes = int(input('Maximo número de rutas de respuesta: '))
+
+        elif int(inputs) == 4:
+            #Requerimiento sin parametros#
+            pass
+
+        elif int(inputs) == 5:
+            initStation = input('Estación de inicio: ')
+            finitStation = input('Estacion de destino: ')
+
+        elif int(inputs) == 6:
+            initDate = input('Fecha inicial de consulta: ')
+            finitDate = input('Fecha final de consulta: ')
+
+        elif int(inputs) == 7:
+            bike_id = int(input('Identificador de la bicicleta en el sistema: '))
+
+        elif int(inputs) == 8:
+            nameStation = input('Nombre de la estacion: ')
+            initDate = input('Fecha inicial de consulta: ')
+            finitDate = input('Fecha final de consulta: ')
+
+        else:
+            sys.exit(0)
+    sys.exit(0)
 
 # Construccion de modelos
 
